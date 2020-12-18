9c880d79345b7cdf1c3efa586da31b103060dffb
Vaidegi <73225161+SK-Vaidegi@users.noreply.github.com>
2020-12-18 19:54:21 +0800

    Add files via upload

---
+++ Capstone Dec 14/Vaidegi Sathish/factorial.java
@@ -0,0 +1,28 @@
+
+
+package factorial;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Karishma
+ */
+public class factorial {
+
+    public static int factorial(int n) {
+        if (n==1) return n;
+        else {
+                n = n * factorial(n-1);
+                }
+        return n;
+    }
+    
+    public static void main(String[] args) {
+        System.out.print("Enter a number to factorial :");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        System.out.println("Factorial of " + num + " = " + factorial(num));
+    }
+}
+
