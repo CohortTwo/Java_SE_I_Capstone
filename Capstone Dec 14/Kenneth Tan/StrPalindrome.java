bd1cdc08d6f627c776c407649ecb1eab12cfb307
Kenneth <73225636+hopliteken@users.noreply.github.com>
2020-12-18 11:29:42 +0800

    Add files via upload

---
+++ Capstone Dec 14/Kenneth Tan/StrPalindrome.java
@@ -0,0 +1,25 @@
+
+public class StrPalindrome {
+    
+     public static void main(String[] args)
+    {
+        // Declaring a string to be reversed
+        String str = "Malayalam";
+        String rev = " "; // Variable to store the reverseed string
+        
+      // looping through the characters starting from the last in the given string
+        for (int i=str.length(); i>0; i--)
+        {
+            rev= rev + str.charAt(i-1);
+        }
+        // Converting the entire String to ower case and display the result
+        str = str.toLowerCase().trim();
+        rev = rev.toLowerCase().trim();
+        
+       if (str.equals(rev))
+            System.out.println(str + " Palindrome");
+        else
+           System.out.println(str + " Not a Palindrome");
+    }
+    
+}
