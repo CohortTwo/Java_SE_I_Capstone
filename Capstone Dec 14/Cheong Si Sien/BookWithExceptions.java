4d6e647efad901b7fd71b799c7d0318367b87700
cheongss <74653353+cheongss@users.noreply.github.com>
2020-12-18 09:34:36 +0800

    Add files via upload

---
+++ Capstone Dec 14/Cheong Si Sien/BookWithExceptions.java
@@ -0,0 +1,89 @@
+package library;
+
+import java.util.Objects;
+
+public class BookWithExceptions {
+
+    private int id;
+    private String title;
+    private int borrowerid;
+    private String author;
+
+    public BookWithExceptions(int id, String title, int borrowerid, String author) {
+        this.id = id;
+        this.title = title;
+        this.borrowerid = borrowerid;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getborrowerid() {
+        return borrowerid;
+    }
+
+    public void setborrowerid(int borrowerid) {
+        this.borrowerid = borrowerid;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", borrower=" + borrower + ", author=" + author + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BookWithExceptions other = (BookWithExceptions) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.borrowerid != other.borrowerid) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        return true;
+    }
+
+}
